77613adfb304b538ee393bbae52f9328cdd817a8
rN <105940969+RenanFonsec@users.noreply.github.com>
2022-06-06 17:24:54 -0300

    Update IMC.py

--- IMC.py
+++ IMC.py
@@ -7,32 +7,47 @@ def imc():
         Resultado['text'] = 'Valor Inválido'
 
 janela = Tk()
-janela.minsize (width=400, height=250)
-janela.maxsize (width=400, height=250)
-janela.config (background= '#979CDB')
 
-IMC = Label(janela, text='Calculo de IMC', font='Arial 20', background='#979CDB')
-Peso = Label(janela, text='Peso', font='Arial 18',background='#979CDB')
-Altura = Label(janela,text='Altura', font='Arial 18',background='#979CDB')
-Resultado = Label(janela,font='Arial 18',background='#979CDB')
+fr1 = Frame (janela,background='#333436')
+fr2 = Frame (janela, background='#333436')
+
+janela.grid_rowconfigure(0, weight=1)
+janela.grid_rowconfigure(1, weight=1)
+janela.grid_rowconfigure(2, weight=1)
+janela.grid_rowconfigure(3, weight=1)
+janela.grid_columnconfigure(0, weight=1)
+janela.grid_columnconfigure(1, weight=1)
+
+janela.minsize (width=300, height=150)
+janela.maxsize (width=1920, height=1080)
+janela.config (background='#333436')
+janela.title ('Calculador de IMC')
 
 
-barra_peso = Entry(janela,font='Arial 14')
-barra_altura = Entry(janela,font='Arial 14')
+
+Peso = Label(fr1, text='Peso', font='Arial 18', background='#333436',foreground='#fff')
+Altura = Label(fr1, text='Altura', font='Arial 18',background='#333436',foreground='#fff')
+Resultado = Label(fr2, font='Arial 18',background='#333436', foreground='#fff')
 
 
-Botao_imc = Button(janela,text='IMC', font='Arial 18', command=imc)
+barra_peso = Entry(fr1, font='Arial 14')
+barra_altura = Entry(fr1, font='Arial 14')
 
 
-barra_peso.grid(row=1,column=1)
-barra_altura.grid(row=2,column=1)
+Botao_imc = Button(fr2, text='IMC', font='Arial 18', command=imc,background='#333436',foreground='#fff')
 
-Peso.grid(row=1,column=0)
-Altura.grid(row=2,column=0)
+fr1.pack()
+fr2.pack()
 
-IMC.grid(row=0,column=1)
-Botao_imc.grid (row=4,column=0)
+barra_peso.grid(row=0,column=1)
+barra_altura.grid(row=1,column=1)
 
-Resultado.grid(row=4,column=1)
+Peso.grid(row=0,column=0)
+Altura.grid(row=1,column=0)
+
+
+Botao_imc.grid (row=0,column=1)
+
+Resultado.grid(row=1,column=1)
 
 janela.mainloop()
